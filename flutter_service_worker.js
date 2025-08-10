b5644df9c8c281ecef9ddf7c61ef661d79676eb5
kevinkobori <kevinkobori@users.noreply.github.com>
2025-08-10 22:42:04 +0000

    deploy: 0727b9677410615e5a3ccfb0ee508ff7d0ff355b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "307d463e2dadc29c7126cd8e354f3083",
+const RESOURCES = {"flutter_bootstrap.js": "d74686e0299efe7e2c71c24127f9b3f1",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
